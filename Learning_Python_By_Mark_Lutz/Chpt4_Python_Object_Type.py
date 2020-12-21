190942f34ec31ddcf50ac8291ae421e3fb46d60f
Yesire-Lincoln <10844889+siyueyinghua@users.noreply.github.com>
2020-12-21 17:39:48 +0800

    Create Chpt4_Python_Object_Type.py
    eample and experiment from <Learnning Python - Powerful object-oriented Programming>

---
+++ Learning_Python_By_Mark_Lutz/Chpt4_Python_Object_Type.py
@@ -0,0 +1,33 @@
+#Python core types
+
+# number 
+sum = 123 + 122
+print(sum)
+print(3.115*2)
+
+import math
+print(math.pi)
+print(math.sqrt(4))
+
+import random
+print(random.random())
+
+# string
+str = 'Spam'
+print(str)
+
+print(str[0])
+print(str[1:4])
+
+str = "SpamAgain"
+print(str)
+
+print(str + "xyz")
+print(str * 8)
+print(str)
+
+L = list(str)
+print(L)
+print(L[1])
+print(L[1:5])
+print(''.join(L))
